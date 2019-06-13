317a67ffb2842ccf2e5fa81adff86f1482f575b7
wym <lvchu_wang@sina.com>
2019-06-13 11:54:52 +0800

    tidy files

--- jsDemo/bindDemo.js
+++ jsDemo/bindDemo.js
@@ -1,4 +1,4 @@
-{
+{ //bind使用小demo
     var obj1 = {
         name: 'obj1'
     }
@@ -11,6 +11,7 @@
     function say() {
         console.log(this.name)
     }
+    //函数绑定obj之后，就不会绑定到别的对象了
     var fn = say.bind(obj1).bind(obj2).bind(obj3)   //obj1
     fn()   
     
@@ -34,16 +35,19 @@
         a: 3
     }
     var bar = function(obj){
+        console.log(obj);
         foo.call(obj);
     }
-
+    
+    //有形参且未传递
     bar();  //undefined
-    bar.call(obj1); //undefined
+    bar.call(obj); //undefined
+
 
     var bar2 = function(){
         foo.call(obj);
     }
-
+    // 此时obj是会在全局作用域中找obj
     bar2(); //2
-    bar2.call(obj1);    //2cx
+    bar2.call(obj1);    //2
 }
